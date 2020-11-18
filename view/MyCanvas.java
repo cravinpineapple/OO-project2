93c36018da65491700785b49c76175ba86d88b6c
Noah Stinson <noahstjohn@ymail.com>
2020-11-17 22:58:45 -0600

    game over rendering

--- view/MyCanvas.java
+++ view/MyCanvas.java
@@ -24,8 +24,19 @@ public class MyCanvas extends JPanel {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-
 		Graphics2D g2 = (Graphics2D) g;
+
+		// displaying game over information
+		if (GameBoard.isGameOver) {
+			String gameOutcomeText = GameBoard.gameWon ? "You Won!" : "You Lost!";
+			Color gameOutcomeColor = GameBoard.gameWon ? Color.blue : Color.red;
+			TextDraw gameOverText = new TextDraw("Game Over, " + gameOutcomeText, 144, 190, gameOutcomeColor, 30);
+			TextDraw scoreText = new TextDraw("Score: " + "XXX", gameOverText.x + 120, gameOverText.y + 30, Color.white, 15);
+			gameOverText.render(g2);
+			scoreText.render(g2);
+
+			return;
+		}
 
 		for (var e: gameElements) {
 			e.render(g2);
