df0d407a34185cf30cf4e6ed840ed7531db8bf09
Noah Stinson <noahstjohn@ymail.com>
2020-11-19 21:14:42 -0600

    added shield/extra shooter

--- model/Shooter.java
+++ model/Shooter.java
@@ -10,9 +10,14 @@ public class Shooter extends GameElement {
 
 	public static final int UNIT_MOVE = 5;
 	public static final int MAX_BULLETS = 3;
+	public static int SPEED_BOOST = 0;
+	public static int EXTRA_BULLETS = 0; 
 
 	private ArrayList<GameElement> components = new ArrayList<>();
 	private ArrayList<GameElement> weapons = new ArrayList<>();
+	private Shield shield;
+	public Shooter extraShooter;
+	private boolean isExtraShooter = false;
 
 	public Shooter(int x, int y) {
 		super(x, y, 0, 0);
@@ -28,22 +33,51 @@ public class Shooter extends GameElement {
 		components.add(s4);
 	}
 
+	// constructor for extra shooter
+	public Shooter(int x, int y, int size) {
+		super(x, y, 0, 0);
+
+		isExtraShooter = true;
+		var s1 = new ShooterElement(x - size, y - size, Color.white, false);    // top left
+
+		components.add(s1);
+	}
+
 	public void moveRight() {
-		super.x += UNIT_MOVE;
+		super.x += UNIT_MOVE + SPEED_BOOST;
 		for (var c: components) {
-			c.x += UNIT_MOVE;
+			c.x += UNIT_MOVE + SPEED_BOOST;
+		}
+
+		if (shield != null) 
+			shield.x += UNIT_MOVE + SPEED_BOOST;
+
+		if (extraShooter != null) {
+			extraShooter.x += UNIT_MOVE + SPEED_BOOST;
+			extraShooter.components.get(0).x += UNIT_MOVE + SPEED_BOOST;
 		}
 	}
 
 	public void moveLeft() {
-		super.x -= UNIT_MOVE;
+		super.x -= UNIT_MOVE + SPEED_BOOST;
 		for (var c: components) {
-			c.x -= UNIT_MOVE;
+			c.x -= UNIT_MOVE + SPEED_BOOST;
+		}
+
+		if (shield != null)
+			shield.x -= UNIT_MOVE + SPEED_BOOST;
+
+		if (extraShooter != null) {
+			extraShooter.x -= UNIT_MOVE + SPEED_BOOST;
+			extraShooter.components.get(0).x -= UNIT_MOVE + SPEED_BOOST;
 		}
 	}
 
 	public boolean canFireMoreBullets() {
-		return weapons.size() < MAX_BULLETS;
+		if (isExtraShooter)
+			return weapons.size() < MAX_BULLETS - 2; 
+
+		return weapons.size() < MAX_BULLETS + EXTRA_BULLETS;
 	}
 
 	public void removeBulletsOutOfBound() {
@@ -59,10 +93,21 @@ public class Shooter extends GameElement {
 
 	@Override
 	public void render(Graphics2D g2) {
+
+		// shield render
+		if (shield != null && !isExtraShooter)
+			shield.render(g2);
+
+		// extra shooter render
+		if (extraShooter != null && !isExtraShooter)
+			extraShooter.render(g2);
+
+		// body render
 		for (var c: components) {
 			c.render(g2);
 		}
 
+		// bullets render
 		for (var w: weapons) {
 			w.render(g2);
 		}
@@ -75,6 +120,26 @@ public class Shooter extends GameElement {
 	public ArrayList<GameElement> getComponents() {
 		return components;
 	}
+
+	// shield power activate
+	public void activateShield() {
+		shield = new Shield(this);
+	}
+
+	// shield power deactivate
+	public void deactivateShield() {
+		shield = null;
+	}
+
+	// extra shooter activate
+	public void activateExtraShooter() {
+		extraShooter = new Shooter(x + ShooterElement.SIZE * 2 + 10, y - 3, ShooterElement.SIZE / 4);
+	}
+	
+	// extra shooter deactivate
+	public void deactivateExtraShooter() {
+		extraShooter = null;
+	}
 	
 	public void checkPlayerComponents() {
 		if (components.isEmpty())
@@ -83,9 +148,25 @@ public class Shooter extends GameElement {
 
 	@Override
 	public void animate() {
+		if (shield != null)
+			shield.animate();
+
 		// calling so each bullet's animate is called  
 		for (var w: weapons) {
 			w.animate();
 		}
+
+		if (extraShooter != null) {
+			for (var w: extraShooter.weapons)
+				w.animate();
+		}
+	}
+
+	public Shield getShield() {
+		return shield;
+	}
+
+	public Shooter getExtraShooter() {
+		return extraShooter;
 	}
 }
