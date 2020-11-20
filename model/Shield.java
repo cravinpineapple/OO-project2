0794aa00c9d985ec0bd292894616af575e38cea1
Noah Stinson <noahstjohn@ymail.com>
2020-11-19 19:15:46 -0600

    shield power up

---
+++ model/Shield.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class Shield extends GameElement {
+
+	// shooter reference so animate w/ shooter
+	private Shooter shooter;
+
+	public Shield(Shooter shooter) {
+		this.shooter = shooter;
+
+		filled = true;
+		x = shooter.x - 30;
+		y = shooter.y - 40;
+		width = ShooterElement.SIZE * 2 + 20;
+		height = 5;
+		color = Color.cyan;
+	}
+
+	@Override
+	public void render(Graphics2D g2) {
+		g2.setColor(color);
+		if (filled) {
+			g2.fillRect(x, y, width, height);
+		}
+		else {
+			g2.drawRect(x, y, width, height);
+		}
+	}
+
+	// animation done with moveRight / moveLeft in Shooter.java
+	@Override
+	public void animate() {
+		// x = shooter.x - 30;
+		// y = shooter.y - 40;
+	}
+	
+}
