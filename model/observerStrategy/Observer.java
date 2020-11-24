278d5e04d600dcc2cd61db9dae5566f14c837f16
Noah Stinson <noahstjohn@ymail.com>
2020-11-24 10:34:47 -0600

    observer interface

---
+++ model/observerStrategy/Observer.java
@@ -0,0 +1,10 @@
+package model.observerStrategy;
+
+// Observer defines an updating interface for objects that should be notified
+// 		of changes in a subject
+
+public interface Observer {
+
+	public void actionPerformed(boolean hasPower);
+
+}
