34a7c96f6199722789c927dabe597fd07634155e
Noah Stinson <noahstjohn@ymail.com>
2020-11-28 09:58:28 -0600

    added bufferedimage variable & new constructor

--- model/GameElement.java
+++ model/GameElement.java
@@ -2,9 +2,12 @@ package model;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 
 public abstract class GameElement {
+
+	protected BufferedImage image;
 	
 	public int x;
 	public int y;
@@ -22,6 +25,17 @@ public abstract class GameElement {
 		this.height = height;
 	}
 
+	// constructor w/ image
+	public GameElement(int x, int y, Color color, boolean filled, int width, int height, BufferedImage image) {
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		this.filled = filled;
+		this.width = width;
+		this.height = height;
+		this.image = image;
+	}
+
 	public GameElement() {
 		this(0, 0, Color.white, false, 0, 0);
 	}
@@ -37,6 +51,10 @@ public abstract class GameElement {
 		}
 		else 
 			return true;
+	}
+
+	public void setImage(BufferedImage image) {
+		this.image = image;
 	}
 
 	public abstract void render(Graphics2D g2);
