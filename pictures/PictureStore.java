21d92425bf69b6299fc314f3364c295b54075f51
Noah Stinson <noahstjohn@ymail.com>
2020-12-01 14:39:32 -0600

    added misc graphics to store

--- pictures/PictureStore.java
+++ pictures/PictureStore.java
@@ -10,6 +10,7 @@ import javax.imageio.ImageIO;
 
 public class PictureStore {
 	
+	// enemies
 	public static BufferedImage angryStrawberry;
 	public static BufferedImage normalStrawberry;
 	public static BufferedImage angryCarrot;
@@ -18,21 +19,33 @@ public class PictureStore {
 	public static BufferedImage normalOnion;
 	public static BufferedImage angryRadish;
 	public static BufferedImage normalRadish;
+
+	// shooter
 	public static BufferedImage tLeftCookie; // top left
 	public static BufferedImage tRightCookie; // top right
 	public static BufferedImage bLeftCookie; // bottom left
 	public static BufferedImage bRightCookie; // bottom right
 	public static BufferedImage fullCookie; // extra shooter
 
+	// other
+	public static BufferedImage bullet;
+	public static BufferedImage bomb;
+	public static BufferedImage speedPower;
+	public static BufferedImage bulletPower;
+	public static BufferedImage shieldPower;
+	public static BufferedImage shooterPower;
+
 
 	public static int[] playerXY = {60, 60};
 	public static int[] extraShooterXY = {30, 30};
 	public static int[] enemyXY = {45, 45};
+	public static int[] bulletXY = {20, 20};
+	public static int[] bombXY = {10, 20};
+	public static int[] powerXY = {30, 30};
 	
 	
 	// executed before main method
 	static {
-		String cwd = System.getProperty("user.dir");
 		angryStrawberry = readImage("pictures/enemies/angryStrawberry.png", enemyXY[0], enemyXY[1]);
 		normalStrawberry = readImage("pictures/enemies/normalStrawberry.png", enemyXY[0], enemyXY[1]);
 		angryCarrot = readImage("pictures/enemies/angryCarrot.png", enemyXY[0], enemyXY[1]);
@@ -46,6 +59,12 @@ public class PictureStore {
 		bLeftCookie = readImage("pictures/shooter/bLeftCookie.png", playerXY[0], playerXY[1]);
 		bRightCookie = readImage("pictures/shooter/bRightCookie.png", playerXY[0], playerXY[1]);
 		fullCookie = readImage("pictures/shooter/fullCookie.png", extraShooterXY[0], extraShooterXY[1]);
+		bomb = readImage("pictures/other/bomb.png", bombXY[0], bombXY[1]);
+		bullet = readImage("pictures/other/bullet.png", bulletXY[0], bulletXY[1]);
+		speedPower = readImage("pictures/other/speedPower.png", powerXY[0], powerXY[1]);
+		bulletPower = readImage("pictures/other/bulletPower.png", powerXY[0], powerXY[1]);
+		shieldPower = readImage("pictures/other/shieldPower.png", powerXY[0], powerXY[1]);
+		shooterPower = readImage("pictures/other/shooterPower.png", powerXY[0], powerXY[1]);
 	}
 
 	// reads from file and scales to width & height
